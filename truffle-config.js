20c7a8ce28ef5ffd4749aa7bacafa663cd5cc784
Miguel <miguelsorinpro@gmail.com>
2021-07-18 17:08:24 +0800

    config changed

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const mnemonic = require("./secret.json").mnemonic
 
 module.exports = {
   /**
@@ -35,8 +35,31 @@ module.exports = {
    * $ truffle test --network <network-name>
    */
 
+   networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard BSC port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    test: {
+      provider: () => new HDWalletProvider(mnemonic, `https://speedy-nodes-nyc.moralis.io/f39b6beef8a97ae9d6712bc6/bsc/testnet`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
 
   // Set default mocha options here, use special reporters etc.
+
+
   // Configure your compilers
   compilers: {
     solc: {
